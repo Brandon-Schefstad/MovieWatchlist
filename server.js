fbca122d3a519caed46c5b8389f271693c3c40e9
Brandon-Schefstad <bschefstad@gmail.com>
2022-03-03 04:27:10 -0500

    Adding first template

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
-
 const app = express()
 const PORT = 8000
 
 app.use(cors())
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(express.static('.'));
 
-
-let squirtle = {
-    'Type':'Water',
-    'Evolution':'Wartortle',
-    'Weaknesses':'Grass, Electric',
-}
+// Get request for loading main page with HTML/EJS
 app.get('', (request, response) => {
-    // response.send('GET request to homepage')
     response.sendFile(__dirname + '/index.html')
-    
 })
 
 app.get('/api/squirtle', (request, response) => {
