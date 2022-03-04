124d7b6ef324c756693566f2889a17891554982a
Brandon-Schefstad <bschefstad@gmail.com>
2022-03-03 19:18:02 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('.'));
-// app.set('view engine','ejs')
+app.set('view engine','ejs')
 
 
 // Read request for loading main page with HTML/EJS
